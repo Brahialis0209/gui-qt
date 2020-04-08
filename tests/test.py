5746c34a832fbc2dd72901d5d102ec822bcbef58
Brahialis0209 <rafantem@mail.ru>
2020-04-08 23:45:57 +0400

    add unittests

--- tests/test.py
+++ tests/test.py
@@ -1,13 +1,20 @@
 import sys
+import unittest
 
 sys.path.append('../')
 
 from tests.dates.test_dates import give_second_test_dates, give_first_test_dates
-from tests.logic.example import solve_example
+from tests.example import solve_example
 
-if __name__ == "__main__":
-    example_first_dates = give_first_test_dates()
-    assert (solve_example(*example_first_dates) == 48.5)
 
-    example_second_dates = give_second_test_dates()
-    assert (solve_example(*example_second_dates) == 16.4)
+class TestTraversalGraph(unittest.TestCase):
+
+    def test_first(self):
+        self.assertEqual(solve_example(*give_first_test_dates()), 48.5)
+
+    def test_second(self):
+        self.assertEqual(solve_example(*give_second_test_dates()), 16.4)
+
+
+if __name__ == '__main__':
+    unittest.main()
